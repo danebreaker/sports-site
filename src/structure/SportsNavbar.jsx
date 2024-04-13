147c7a2602e6d328ca5b079d2b713e335319b6c0
TheDanahe <149431735+danebreaker@users.noreply.github.com>
2024-04-13 00:30:42 -0500

    Update SportsNavbar.jsx

--- src/structure/SportsNavbar.jsx
+++ src/structure/SportsNavbar.jsx
@@ -1,9 +1,12 @@
 import { Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import '../App.css';
+import { useState } from "react";
 
 export default function SportsNavbar(props) {
-    return <Navbar fixed="top" expand="lg" className="bg-body-tertiary" bg="dark" data-bs-theme="dark">
+    const [expanded, setExpanded] = useState(false);
+
+    return <Navbar fixed="top" expand="lg" className="bg-body-tertiary" collapseOnSelect={true} bg="dark" data-bs-theme="dark">
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
