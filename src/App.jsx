5d445c346298d5aadb03292406b63b467b987cba
TheDanahe <149431735+danebreaker@users.noreply.github.com>
2024-04-15 09:16:37 -0500

    added analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import SportsRouter from './structure/SportsRouter';
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return <>
     <SportsRouter />
+    <SpeedInsights />
+    <Analytics />
   </>
 }
 
