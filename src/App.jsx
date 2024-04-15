83344c18849a1b9475934baa32b147fecb372039
TheDanahe <149431735+danebreaker@users.noreply.github.com>
2024-04-15 09:20:39 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import SportsRouter from './structure/SportsRouter';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import { Analytics } from "@vercel/analytics/react"
 
 function App() {
